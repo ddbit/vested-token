d2ca6310227fa14b7331beb5e05b60830b837c7d
davide <davide@Mini-di-davide.lan>
2022-06-22 21:58:58 +0200

    commit contracts

---
+++ contracts/VestedToken.sol
@@ -0,0 +1,109 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+import "hardhat/console.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
+
+contract VestedToken is IERC20, IERC20Metadata {
+    address public token;
+    address public beneficiary;
+    address public emitter;
+    uint256 public timelock;
+
+    constructor(address _token, 
+                address _beneficiary, 
+                address _emitter,
+                uint256 _timelock) {
+        token = _token;
+        beneficiary = _beneficiary;
+        emitter = _emitter;
+        timelock = _timelock;
+
+    }
+
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external virtual override view returns (string memory){
+        return string.concat(IERC20Metadata(token).name(),"_locked");
+    }
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external virtual override view returns (string memory){
+        return string.concat(IERC20Metadata(token).symbol(),"lk");
+    }
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external virtual override view returns (uint8){
+        return IERC20Metadata(token).decimals();
+    }
+
+
+
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external virtual override view returns (uint256){
+        return IERC20(token).balanceOf(address(this));
+    }
+
+   /**
+     * @dev Returns the amount of tokens owned by `account`.
+     * this function returns the tokens locked in this contract if account is
+     * the beneficiary, otherwise zero.
+     * 
+     */
+    function balanceOf(address account) external virtual override view returns (uint256){
+        if(account == beneficiary)
+            return IERC20(token).balanceOf(address(this));
+        else return (0);
+    }
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `to`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address to, uint256 amount) external virtual override returns (bool){
+        require(block.timestamp > timelock, "Timelock still in place");
+        emit Transfer(beneficiary, to, amount);
+        bool retval = IERC20(token).transfer(to, amount);
+        if(retval==false) revert();
+        return retval;
+    }
+
+    /**
+     * @dev approve and allowances are disabled for this contract
+     */
+    function allowance(address owner, address spender) external virtual override view returns (uint256){
+        return 0;
+    }
+
+    /**
+     * @dev approve( ) is disabled for this contract
+     */
+    function approve(address spender, uint256 amount) external virtual override returns (bool){
+        return false;
+    }
+
+    /**
+     * @dev transferFrom is disabled for this contract
+     */
+    function transferFrom(
+        address from,
+        address to,
+        uint256 amount
+    ) external virtual override returns (bool){
+        return false;
+    }
+
+
+}
